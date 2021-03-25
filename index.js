427f627d3f157e48b0ac7630c7041622f134d8ee
orpip <farihatabassumorpita@hotmail.com>
2021-03-25 13:52:19 +0600

    added doodle feature

--- index.js
+++ index.js
@@ -1,10 +1,52 @@
 let containerDiv = document.getElementById("container")
+let click = false
 
-function createGrid(rows, cols){
+function createGrid(length){
     
-    for(let i=0; i<(rows*cols); i++){
-        let divs = document.createElement("div")
-        containerDiv.appendChild(divs).className = "cell"
+    for(let i=0; i<(length ** 2); i++){                       
+        //loops upto the number of total cells
+
+        let cell = document.createElement("div")
+        //creates a div each time
+
+        containerDiv.appendChild(cell).className = "cell"
+        //appends the cells to container div
+
+        if(cell){
+          let fill = function(){
+            cell.style.backgroundColor = "black"
+          }
+          
+          cell.addEventListener("mousedown", () =>{
+            click = true
+
+            cell.style.backgroundColor = "black"
+          })
+          //when mouse is held, click becomes true bgcolor is set again to fill the cell even
+          //when mouse isn't held down, but also works with just a click
+          
+          cell.addEventListener("mouseup", () =>{
+            click = false
+          })
+          //when mouse is released, click becomes false
+          
+          cell.addEventListener("mouseenter", () => {
+            if(click){
+              cell.style.backgroundColor = "black"
+            }
+          })
+          //this activates when the mouse enters the cells and doodles only when click is true
+          
+          }
     }
+
+    let containerSize = 512;
+    let cellSize = containerSize/length;
+    //determines the size of the cell so that all of it fits
+    //equally into the container
+
+    containerDiv.style.gridTemplateColumns = `repeat(${length}, ${cellSize}px)`;
+    containerDiv.style.gridTemplateRows = `repeat( ${length}, ${cellSize}px)`;
 }
-createGrid(16, 16);
+
+createGrid(64, 64);
